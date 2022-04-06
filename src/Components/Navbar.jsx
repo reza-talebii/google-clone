cda6915a9988524dcb41d2ebd509610e0314b621
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-06 10:52:40 +0430

    add route pakage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+const styles = {
+  wrapper:
+    "p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200",
+  navStyle: "flex justify-between items-center space-x-5 w-screen",
+};
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <section className={styles.wrapper}>
+      <nav className={styles.navStyle}>
+        <Link to="/">home</Link>
+      </nav>
+    </section>
+  );
 };
 
 export default Navbar;
