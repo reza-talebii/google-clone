d19e03d0bf2f9de88c4bc9cb59513dc7bb57910f
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-06 14:02:17 +0430

    complete Routes file

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Routes as RouteWarper, Route, Redirect } from "react-router-dom";
 
 const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <section className="p-4">
+      <RouteWarper>
+        <Route path="/">
+          <Redirect to="/search" />
+        </Route>
+
+        <Route path={["/search", "/images", "/news"]} element={<Result />} />
+      </RouteWarper>
+    </section>
+  );
 };
 
 export default Routes;
