01b4b61c91a41f76d52bdf2756f1481a97aa69bf
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-06 18:18:35 +0430

    complete news result

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 import { useResultContext } from "../Context/store";
 
-import { Loading, SearchResult, Error } from "./";
+import { Loading, SearchResult, Error, ImageResult, NewsResult } from "./";
 
 const styles = {
   searchContainer: "flex flex-wrap justify-between space-y-6 sm:px-56",
   imagesContainer: "flex flex-wrap justify-center items-center",
+  newContainer:
+    "flex flex-wrap justify-between space-y-6 sm:px-56 items-center",
 };
 
 const Results = () => {
@@ -15,29 +17,43 @@ const Results = () => {
   const { result, error, loading, getResults } = useResultContext();
 
   useEffect(() => {
-    // getResults("search/q=elon+musk");
+    // getResults(`${pathname}/q=tesla&num=20`);
   }, []);
 
   if (loading) return <Loading />;
   if (error) return <Error message={error} />;
 
+  console.log(result);
+
   switch (pathname) {
     case "/search":
       return (
         <div className={styles.searchContainer}>
-          {result?.results?.map((result, i) => (
+          {result?.map((result, i) => (
             <SearchResult result={result} key={i} />
           ))}
         </div>
       );
       break;
 
-    case "/images":
-      return <div className={styles.imagesContainer}>{result.results}</div>;
+    case "/image":
+      return (
+        <div className={styles.imagesContainer}>
+          {result?.map((res, i) => (
+            <ImageResult result={res} key={i} />
+          ))}
+        </div>
+      );
       break;
 
     case "/news":
-      return <div className={styles.searchContainer}></div>;
+      return (
+        <div className={styles.newContainer}>
+          {result?.map((result, i) => (
+            <NewsResult result={result} key={i} />
+          ))}
+        </div>
+      );
       break;
 
     case "/videos":
@@ -45,6 +61,7 @@ const Results = () => {
       break;
 
     default:
+      <Error massage="not found" />;
       break;
   }
 };
