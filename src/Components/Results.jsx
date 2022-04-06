4eb734f4a5832b22d1299217249307094f0f1396
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-06 15:06:33 +0430

    get data in api

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useResultContext } from "../Context/store";
 
 const Results = () => {
+  const { getResults } = useResultContext();
+
+  useEffect(() => {
+    getResults("search/q=elon+musk");
+  }, [getResults]);
+
   return <div>Results</div>;
 };
 
--- src/Context/resultProvider.js
+++ src/Context/resultProvider.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import store from "./store";
+
+const BASE_URL = "https://google-search3.p.rapidapi.com/api/v1";
 
 const ResultProvider = ({ children }) => {
   const [result, setResult] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  //fetch data in api
+  const getResults = async (type) => {
+    setLoading(true);
+    setError(true);
+
+    const res = await fetch(`${BASE_URL}/${type}`, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-Proxy-Location": "EU",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": "449ece1bedmsh44664fa38cb935ap17810ejsn3d3db2fe6ce0",
+      },
+    });
+    const data = await res.json();
+
+    res.ok ? setResult(data) : setError(res.statusText);
+
+    setLoading(false);
+  };
+
   return (
-    <store.Provider value={{ result, error, loading }}>
+    <store.Provider value={{ result, error, loading, getResults }}>
       {children}
     </store.Provider>
   );
