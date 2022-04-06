35cacfaaa955b61e8d471f03b1d87cc770cbf818
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-06 14:26:15 +0430

    complete Router comp

--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Routes as RouteWarper, Route, Redirect } from "react-router-dom";
+import { Routes as RouteWarper, Route, Navigate } from "react-router-dom";
+
+import { Results } from "./";
 
 const Routes = () => {
   return (
     <section className="p-4">
       <RouteWarper>
-        <Route path="/">
-          <Redirect to="/search" />
-        </Route>
-
-        <Route path={["/search", "/images", "/news"]} element={<Result />} />
+        <Route path="/" element={<Navigate replace to="/search" />} />
+        <Route path="/search" element={<Results />} />
+        <Route path="/images" element={<Results />} />
+        <Route path="/video" element={<Results />} />
+        <Route path="/news" element={<Results />} />
+        <Route path="*" element={<Results />} />
       </RouteWarper>
     </section>
   );
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as Routes } from "./Routes";
 export { default as Search } from "./Search";
+export { default as Results } from "./Results";
